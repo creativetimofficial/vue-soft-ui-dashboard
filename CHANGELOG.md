1dd59ec811dcd2bab21a82ba4d33e04d6d55163a
sajadevo <nawabisajadahmad@gmail.com>
2022-01-27 15:13:00 +0430

    Fix the dashboard issue and add readme files

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+## [1.0.0] 2022-01-27
+
+### Initial Release
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -32,7 +32,7 @@
           directionReverse
         ></card>
       </div>
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+      <div class="col-xl-3 col-sm-6 mb-xl-0">
         <card
           :title="stats.sales.title"
           :value="stats.sales.value"
@@ -42,83 +42,83 @@
           directionReverse
         ></card>
       </div>
-      <div class="row">
-        <div class="col-lg-7 mb-lg-0 mb-4">
-          <div class="card">
-            <div class="card-body p-3">
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="d-flex flex-column h-100">
-                    <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                    <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
-                    <p class="mb-5">
-                      From colors, cards, typography to complex elements, you
-                      will find the full documentation.
-                    </p>
-                    <a
-                      class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-                      href="javascript:;"
-                    >
-                      Read More
-                      <i
-                        class="fas fa-arrow-right text-sm ms-1"
-                        aria-hidden="true"
-                      ></i>
-                    </a>
-                  </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-7 mb-lg-0 mb-4">
+        <div class="card">
+          <div class="card-body p-3">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="d-flex flex-column h-100">
+                  <p class="mb-1 pt-2 text-bold">Built by developers</p>
+                  <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
+                  <p class="mb-5">
+                    From colors, cards, typography to complex elements, you will
+                    find the full documentation.
+                  </p>
+                  <a
+                    class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto"
+                    href="javascript:;"
+                  >
+                    Read More
+                    <i
+                      class="fas fa-arrow-right text-sm ms-1"
+                      aria-hidden="true"
+                    ></i>
+                  </a>
                 </div>
-                <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
-                  <div class="bg-gradient-success border-radius-lg h-100">
+              </div>
+              <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
+                <div class="bg-gradient-success border-radius-lg h-100">
+                  <img
+                    src="../assets/img/shapes/waves-white.svg"
+                    class="position-absolute h-100 w-50 top-0 d-lg-block d-none"
+                    alt="waves"
+                  />
+                  <div
+                    class="position-relative d-flex align-items-center justify-content-center h-100"
+                  >
                     <img
-                      src="../assets/img/shapes/waves-white.svg"
-                      class="position-absolute h-100 w-50 top-0 d-lg-block d-none"
-                      alt="waves"
+                      class="w-100 position-relative z-index-2 pt-4"
+                      src="../assets/img/illustrations/rocket-white.png"
+                      alt="rocket"
                     />
-                    <div
-                      class="position-relative d-flex align-items-center justify-content-center h-100"
-                    >
-                      <img
-                        class="w-100 position-relative z-index-2 pt-4"
-                        src="../assets/img/illustrations/rocket-white.png"
-                        alt="rocket"
-                      />
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-5">
-          <div class="card h-100 p-3">
+      </div>
+      <div class="col-lg-5">
+        <div class="card h-100 p-3">
+          <div
+            class="overflow-hidden position-relative border-radius-lg bg-cover h-100"
+            style="
+              background-image: url('https://demos.creative-tim.com/soft-ui-dashboard/assets/img/ivancik.jpg');
+            "
+          >
+            <span class="mask bg-gradient-dark"></span>
             <div
-              class="overflow-hidden position-relative border-radius-lg bg-cover h-100"
-              style="
-                background-image: url('https://demos.creative-tim.com/soft-ui-dashboard/assets/img/ivancik.jpg');
-              "
+              class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3"
             >
-              <span class="mask bg-gradient-dark"></span>
-              <div
-                class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3"
+              <h5 class="text-white font-weight-bolder mb-4 pt-2">
+                Work with the rockets
+              </h5>
+              <p class="text-white">
+                Wealth creation is an evolutionarily recent positive-sum game.
+                It is all about who take the opportunity first.
+              </p>
+              <a
+                class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto"
+                href="javascript:;"
               >
-                <h5 class="text-white font-weight-bolder mb-4 pt-2">
-                  Work with the rockets
-                </h5>
-                <p class="text-white">
-                  Wealth creation is an evolutionarily recent positive-sum game.
-                  It is all about who take the opportunity first.
-                </p>
-                <a
-                  class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-                  href="javascript:;"
-                >
-                  Read More
-                  <i
-                    class="fas fa-arrow-right text-sm ms-1"
-                    aria-hidden="true"
-                  ></i>
-                </a>
-              </div>
+                Read More
+                <i
+                  class="fas fa-arrow-right text-sm ms-1"
+                  aria-hidden="true"
+                ></i>
+              </a>
             </div>
           </div>
         </div>
